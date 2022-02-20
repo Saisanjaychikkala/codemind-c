c7c89b6a6a4108760325ffd2c7054416c9cc133d
Saisanjaychikkala <97425672+Saisanjaychikkala@users.noreply.github.com>
2022-02-20 21:20:38 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         int n1=0,n2=0;         scanf("%d %d",&n1,&n2);         printf("%d",n1+n2);         printf(" ");     }     return 0; }

---
+++ sum_of_the_given_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        int n1=0,n2=0;
+        scanf("%d %d",&n1,&n2);
+        printf("%d",n1+n2);
+        printf("
+");
+    }
+    return 0;
+}
